cf3d1824146aa02dc6af19a67107e72f4b36be1d
Paweł Muszyński <p.muszynski200@gmail.com>
2025-03-22 16:58:27 +0100

    Added CORS

---
+++ backend/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
+from apis.notes import router as notes_router
+from database import SessionLocal, engine
+import models as models
+
+# Tworzymy tabele w bazie danych
+models.Base.metadata.create_all(bind=engine)
+
+# Tworzymy aplikację FastAPI
+app = FastAPI()
+
+# Dodajemy middleware CORS
+origins = [
+    "http://localhost:8081",  
+    "http://127.0.0.1:8081", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],
+)
+
+# Rejestrujemy routers dla użytkowników i notatek
+app.include_router(users_router)
+app.include_router(notes_router)
